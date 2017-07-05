1c014013fb9518e07ee9ce6923bc9be6afc0e2ec
bellawu <xjtu_beiwu@163.com>
2017-07-05 10:57:32 +0800

    test3

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello github");
+        System.out.println("test again");
     }
 }
